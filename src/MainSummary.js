a52a7a59243a2fb0fc2c24c15ec6c8d6f58acf1f
Audreycious <porcupine4@gmail.com>
2019-08-28 23:18:33 -0500

    working and ready to move css

--- src/MainSummary.js
+++ src/MainSummary.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SummaryOption from "./SummaryOption";
 
 
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
@@ -10,21 +11,14 @@ class MainSummary extends Component {
     static defaultProps = { features: [], selected: [] };
     render() {
         const features = this.props.features;
-        const summary = Object.keys(this.props.selected).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
+        const summary = Object.keys(this.props.selected).map((feature, i) => {
+            const featureHash = feature + '-' + i;
             const selectedOption = this.props.selected[feature];
 
             return (
-            <div className="summary__option" key={featureHash}>
-                <div className="summary__option__label">{feature} </div>
-                <div className="summary__option__value">{selectedOption.name}</div>
-                <div className="summary__option__cost">
-                {USCurrencyFormat.format(selectedOption.cost)}
-                </div>
-            </div>
+                <SummaryOption key={featureHash} featureHash={featureHash} selectedOption={selectedOption} feature={feature} />
             );
         });
-
         const total = Object.keys(this.props.selected).reduce(
             (acc, curr) => acc + this.props.selected[curr].cost,
             0
